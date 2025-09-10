c1872750fbda7436080f27c48d381956dc8193c8
João Ferrenha <jv_ferrenha@hotmail.com>
2025-09-09 22:08:36 -0300

    Semana 2 - API inicial com login, registro e listagem de usuários

---
+++ Semana2/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
